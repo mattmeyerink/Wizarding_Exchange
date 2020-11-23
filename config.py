c8f1dbe565ddaa003b928743c4eea1757facca68
mattmeyerink <meyerink@umich.edu>
2020-11-23 15:54:52 -0600

    Adding db settings

--- config.py
+++ config.py
@@ -7,4 +7,8 @@ load_dotenv(os.path.join(basedir, ".env"))
 class Config:
     FLASK_APP = os.getenv("FLASK_APP")
     FLASK_ENV = os.getenv("FLASK_ENV")
+
     SECRET_KEY = os.getenv("SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
