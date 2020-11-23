2830d412ef44a82b9191146dc286d2e8c5346490
mattmeyerink <meyerink@umich.edu>
2020-11-23 15:40:44 -0600

    More routing for shop and displaying products

--- app/blueprints/shop/routes.py
+++ app/blueprints/shop/routes.py
@@ -1,8 +1,22 @@
 import flask
+from .models import Product
 from . import shop_bp
 
 
 @shop_bp.route("/")
 def show_all_items():
     """Displays all of the items currently listed in the shop."""
-    return flask.render_template("item_display.html")
+    context = {
+        "category": "All Products",
+        "products": Product.query.all()
+    }
+    return flask.render_template("item_display.html", **context)
+
+@shop_bp.route("/<category>")
+def show_category(category):
+    """Displays all of the items of a specific category."""
+    context = {
+        "category": category.title(),
+        "products": Product.query.filter_by(category=category).all()
+    }
+    return flask.render_template("item_display.html", **context)
